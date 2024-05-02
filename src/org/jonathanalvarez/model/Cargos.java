72630af8f7cb87049f616f2e68b27601dfc2cb51
Informatica <informatica@alumno.local>
2024-05-02 16:58:33 -0600

    Entrega 3

--- src/org/jonathanalvarez/model/Cargos.java
+++ src/org/jonathanalvarez/model/Cargos.java
@@ -10,5 +10,47 @@ package org.jonathanalvarez.model;
  * @author informatica
  */
 public class Cargos {
+    private int cargoId;
+    private int nombreCargo;
+    private int descripcionCargo;
+
+    public Cargos() {
+    }
+
+    public Cargos(int cargoId, int nombreCargo, int descripcionCargo) {
+        this.cargoId = cargoId;
+        this.nombreCargo = nombreCargo;
+        this.descripcionCargo = descripcionCargo;
+    }
+
+    public int getCargoId() {
+        return cargoId;
+    }
+
+    public void setCargoId(int cargoId) {
+        this.cargoId = cargoId;
+    }
+
+    public int getNombreCargo() {
+        return nombreCargo;
+    }
+
+    public void setNombreCargo(int nombreCargo) {
+        this.nombreCargo = nombreCargo;
+    }
+
+    public int getDescripcionCargo() {
+        return descripcionCargo;
+    }
+
+    public void setDescripcionCargo(int descripcionCargo) {
+        this.descripcionCargo = descripcionCargo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cargos{" + "cargoId=" + cargoId + ", nombreCargo=" + nombreCargo + ", descripcionCargo=" + descripcionCargo + '}';
+    }
+    
     
 }
